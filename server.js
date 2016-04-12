a0e490305d2235260ce6d67cd366d88d3ef07a0b
Student <Student@Mac-mini-2.local>
2016-04-12 13:33:04 -0600

    styling update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var express     = require('express'),
     app.use(bodyP.urlencoded({extended: true}))
     app.use(cors())
     // app.use('/api/v1', apiRoutes)
-    app.use(express.static(path.join(__dirname, './public')))
+    app.use(express.static(__dirname + '/public'))
 
     app.listen(port, function(err){
       if(err) console.log(err)
